1f648c64cd84420c09a7bde022673ac1f9a25056
Fernando Lemos <FernandoLemos@hotmail.com.br>
2023-11-19 10:59:09 -0300

    add @types and theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,22 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import * as Style from "./styles";
+import * as theme from "src/theme";
 
 export function Groups() {
   return (
-    <View style={styles.container}>
-      <Text style = {styles.text}>Groups</Text>
-    </View>
+    <theme.Container>
+      <Style.Title> Groups </Style.Title>
+    </theme.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 32
-  },
-});
